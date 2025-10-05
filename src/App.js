a3faee71c17d1d2f2e3d5c374273202718a811a1
ermiasdesalegn <ermiasdesalegnn@gmail.com>
2025-10-05 23:41:25 +0300

    project structure for bill and percent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+
+import { useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Bill />
+      <Percentage />
+    </div>
+  );
+}
+function Bill() {
+  const [bill, setBill] = useState(0);
+  return (
+    <div>
+      <p></p>
+      {"How much was the bill? "}
+      <input
+        type="number"
+        value={bill}
+        onChange={(e) => setBill(e.target.value)}
+      />
+    </div>
+  );
+}
+function Percentage({ placeholder }) {
+  const [percent, setPercent] = useState(0);
+  return (
+    <div>
+      {placeholder}
+      <select>
+        <option value={0}>Dissatisfied(0%)</option>
+        <option value={5}>it was okay (5%)</option>
+        <option value={10}>it was good (10%)</option>
+        <option value={20}>Absolutely amazing (20%)</option>
+      </select>
     </div>
   );
 }
