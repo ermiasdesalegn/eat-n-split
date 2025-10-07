55eb8ead418dcf54ef52854edc31e88969f81556
ermiasdesalegnn <ermiasdesalegnn@gmail.com>
2025-10-07 22:47:08 +0300

    friend component is enhanced and created button

--- src/App.js
+++ src/App.js
@@ -43,6 +43,20 @@ function Friend({ friend }) {
     <li>
       <img src={friend.name} alt={friend.name} />
       <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance === 0 && (
+        <p className="">You and {friend.name} are even</p>
+      )}
+      <button className="button">Select</button>
     </li>
   );
 }
