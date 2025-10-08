be9fa855b15dc4441d534b9c3ba619e63b834e0e
ermiasdesalegnn <ermiasdesalegnn@gmail.com>
2025-10-08 21:56:58 +0300

    button and also fields being controlled now added

--- src/App.js
+++ src/App.js
@@ -104,8 +104,22 @@ function FormSplitBill() {
 function FriendForm() {
   const [name, setName] = useState("");
   const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+    console.log(newFriend);
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>🧑‍🤝‍🧑Friend name</label>
       <input
         type="text"
